27fdc374bb35cd6ecea801dccae271bea0a1033b
CovenantDev <john.t.rathmell@gmail.com>
2024-03-04 16:01:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Another public test...");
+        }
+    }
+}
